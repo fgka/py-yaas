b687ec29207a9318a9300a2b1814492c1757eed3
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2023-01-10 00:34:09 +0100

    missed output.tf

--- terraform/2_cicd_build/output.tf
+++ terraform/2_cicd_build/output.tf
@@ -7,10 +7,6 @@ output "python_build_trigger" {
   value = google_cloudbuild_trigger.python
 }
 
-output "docker_base_build_trigger" {
-  value = google_cloudbuild_trigger.docker_base
-}
-
-output "docker_app_build_trigger" {
-  value = google_cloudbuild_trigger.docker_app
+output "docker_build_trigger" {
+  value = google_cloudbuild_trigger.docker
 }

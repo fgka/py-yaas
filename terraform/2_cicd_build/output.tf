3ab82134690f58ecf25797ec21e11444b75e2e1f
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2023-01-09 23:56:42 +0100

    Added missing outputs on build TF

--- terraform/2_cicd_build/output.tf
+++ terraform/2_cicd_build/output.tf
@@ -6,3 +6,11 @@
 output "python_build_trigger" {
   value = google_cloudbuild_trigger.python
 }
+
+output "docker_base_build_trigger" {
+  value = google_cloudbuild_trigger.docker_base
+}
+
+output "docker_app_build_trigger" {
+  value = google_cloudbuild_trigger.docker_app
+}

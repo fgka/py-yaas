0d60e0e456e54e2166f8c9d83a3efc0428e9d5a1
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2023-02-21 19:04:34 +0100

    Added alert and config.json outputs

--- terraform/yaas/2_yaas_app/output.tf
+++ terraform/yaas/2_yaas_app/output.tf
@@ -5,17 +5,23 @@
 output "run_yaas" {
   value = google_cloud_run_service.yaas
 }
+/////////////////
+// Config JSON //
+/////////////////
+
+output "local_config_json" {
+  value = local_file.config_json.filename
+}
+
+output "gcs_config_json" {
+  value = "gs://${google_storage_bucket_object.config_json.bucket}/${google_storage_bucket_object.config_json.output_name}"
+}
 
 /////////////////////////////
 // Monitoring and Alerting //
 /////////////////////////////
 
-/*
 output "alert_policy_error_log" {
   value = google_monitoring_alert_policy.alert_error_log
 }
 
-output "alert_policy_not_executed" {
-  value = google_monitoring_alert_policy.alert_not_executed
-}
-*/

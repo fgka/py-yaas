c989113b9872181be1cad2791adc24ec9d7555cc
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2023-03-06 10:55:56 +0100

    Building Python on multiple steps

--- terraform/cicd/2_cicd_build/main.tf
+++ terraform/cicd/2_cicd_build/main.tf
@@ -39,20 +39,20 @@ data "google_service_account" "build_service_account" {
 ////////////////////
 
 // CI/CD itself
-resource "google_cloudbuild_trigger" "tf_build" {
+resource "google_cloudbuild_trigger" "tf_cicd" {
   location           = var.region
   name               = var.tf_build_trigger_name
   service_account    = data.google_service_account.tf_build_service_account.id
   filename           = local.tf_build_template_filename
   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
   substitutions = {
-    _TF_BUCKET_NAME     = var.terraform_bucket_name
-    _TF_MODULE          = "cicd"
-    _TF_BACKEND_TF_TMPL = local.tf_backend_tf_template_filename
-    _BUCKET_NAME        = var.build_bucket_name
-    _TF_PLAN_ARGS       = local.tf_cicd_plan_args_str
-    #TODO _PYTHON_BUILD_TRIGGER_LST = join("@", google_cloudbuild_trigger.python.*.name)
-    _INFRA_BUILD_TRIGGER = google_cloudbuild_trigger.tf_yaas.name
+    _TF_BUCKET_NAME           = var.terraform_bucket_name
+    _TF_MODULE                = "cicd"
+    _TF_BACKEND_TF_TMPL       = local.tf_backend_tf_template_filename
+    _BUCKET_NAME              = var.build_bucket_name
+    _TF_PLAN_ARGS             = local.tf_cicd_plan_args_str
+    _PYTHON_BUILD_TRIGGER_LST = join("@", [for trigger in google_cloudbuild_trigger.python : trigger.name])
+    _INFRA_BUILD_TRIGGER      = google_cloudbuild_trigger.tf_infra.name
   }
   ignored_files = var.tf_build_ignored_files
   included_files = [
@@ -69,7 +69,7 @@ resource "google_cloudbuild_trigger" "tf_build" {
 }
 
 // application infrastructure
-resource "google_cloudbuild_trigger" "tf_yaas" {
+resource "google_cloudbuild_trigger" "tf_infra" {
   location           = var.region
   name               = var.tf_yaas_trigger_name
   service_account    = data.google_service_account.tf_build_service_account.id

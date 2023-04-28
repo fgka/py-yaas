29d7a9cdf0b219be2e88477294889772e1f21ef6
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2023-04-28 16:17:19 +0200

    Added Calendar to factory from config

--- code/core/src/yaas_caching/factory.py
+++ code/core/src/yaas_caching/factory.py
@@ -1,10 +1,47 @@
 # vim: ai:sw=4:ts=4:sta:et:fo=croql
 """Creates the proper py:class:`base.StoreContextManager` instance."""
-from yaas_caching import base, file, gcs
-from yaas_common import logger
+from yaas_caching import base, calendar, file, gcs
+from yaas_common import logger, preprocess
 from yaas_config import config
 
 _LOGGER = logger.get(__name__)
+
+
+def calendar_store_from_cache_config(value: config.CalendarCacheConfig) -> base.ReadOnlyStoreContextManager:
+    """Based on the configuration will create the proper py:class:`base.ReadOnlyStoreContextManager` instance.
+
+    Args:
+        value:
+
+    Returns:
+
+    """
+    # validate input
+    preprocess.validate_type(value, "value", config.CalendarCacheConfig)
+    # logic
+    if value.type == config.CacheType.CALENDAR_API.value:
+        result = calendar.ReadOnlyGoogleCalendarStore(
+            calendar_id=value.calendar_id,
+            secret_name=value.secret_name,
+        )
+    elif value.type == config.CacheType.CALDAV.value:
+        result = calendar.ReadOnlyCalDavStore(
+            caldav_url=value.caldav_url,
+            username=value.username,
+            secret_name=value.secret_name,
+        )
+    elif value.type == config.CacheType.GOOGLE_CALDAV.value:
+        result = calendar.ReadOnlyGoogleCalDavStore(
+            calendar_id=value.calendar_id,
+            username=value.username,
+            secret_name=value.secret_name,
+        )
+    else:
+        raise ValueError(
+            f"Configuration of type {value.type} is not supported. "
+            f"Check implementation of {calendar_store_from_cache_config.__name__} in {__file__}"
+        )
+    return result
 
 
 def store_from_cache_config(value: config.CacheConfig) -> base.StoreContextManager:
@@ -17,8 +54,7 @@ def store_from_cache_config(value: config.CacheConfig) -> base.StoreContextManag
 
     """
     # validate input
-    if not isinstance(value, config.CacheConfig):
-        raise TypeError(f"Argument must be an instance of {config.CacheConfig.__name__}. Got: <{value}>({type(value)})")
+    preprocess.validate_type(value, "value", config.CacheConfig)
     # logic
     if value.type == config.CacheType.LOCAL_JSON_LINE.value:
         result = file.JsonLineFileStoreContextManager(
